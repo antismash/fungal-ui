antismash/fungal-ui-dev

start: fix example output link
Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

app/start/start.js
@@ -157,7 +157,7 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
             }
 
             vm.openSampleOutput = function () {
-                $window.open("/upload/as5-example/index.html", "_blank");
+                $window.open("/upload/fungal-as5-example/index.html", "_blank");
             }
 
             vm.loadJob = function () {

antismash/fungal-ui

app/start/start.js
@@ -194,7 +194,7 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
             }
 
             vm.openSampleOutput = function () {
-                $window.open("/upload/fungal-example/index.html", "_blank");
+                $window.open("/upload/fungal-dev-example/index.html", "_blank");
             }
 
             vm.loadJob = function () {